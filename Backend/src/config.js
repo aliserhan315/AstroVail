d80cbf2b0d5a9ca53e81230ac37dd865fcdecd51
aliserhan315 <aliserhan315@gmail.com>
2025-08-22 15:26:26 +0300

    small edits in the config page

--- Backend/src/config.js
+++ Backend/src/config.js
@@ -4,6 +4,6 @@ dotenv.config();
 
 export const config = {
   port: process.env.PORT || 3000,
-  mongodbUri: process.env.MONGODB_URI || 'mongodb://localhost:27017/astro-vail',
-  jwtSecret: process.env.JWT_SECRET || 'change-me',
+  mongodbUri: process.env.MONGODB_URI ,
+  jwtSecret: process.env.JWT_SECRET,
 };
