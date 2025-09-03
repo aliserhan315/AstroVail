8c8c75756bb12c10b1a2875b077b0bc2c67d6cb8
aliserhan315 <aliserhan315@gmail.com>
2025-09-03 23:07:42 +0300

    created the star screen

---
+++ Frontend/app/(tabs)/search/StarsScreen.styles.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    position: "relative", 
+    paddingTop: 50,
+    paddingHorizontal: 16,
+  },
+  loader: {
+    marginTop: 32,
+  },
+  scrollContent: {
+    paddingBottom: 32,
+  },
+});
---
+++ Frontend/app/(tabs)/search/index.tsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from "react";
+import { ScrollView, View, ActivityIndicator } from "react-native";
+import SearchBar from "@/components/Search/SearchBar";
+import StarsList from "@/components/Search/StarListSearch";
+import Background from "@/components/Background";
+import { StarsAPI } from "@/lib/endpoint";
+import styles from "./StarsScreen.styles";
+import { Star } from "@/components/Home/StarItem";
+import { transformStars  } from "@/lib/utils/formatStar";
+
+export default function StarsScreen() {
+  const [search, setSearch] = useState("");
+  const [stars, setStars] = useState<Star[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  const loadStars = async (query: string) => {
+    try {
+      setLoading(true);
+      const res = await StarsAPI.list({ q: query });
+      setStars(transformStars(res.items));
+    } catch (err) {
+      console.error("Failed to load stars", err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    loadStars("");
+  }, []);
+
+  const handleSearch = (text: string) => setSearch(text);
+  const handleClear = () => {
+    setSearch("");
+    loadStars("");
+  };
+  const handleSubmit = (text: string) => loadStars(text);
+  const handlePressStar = (star: Star) => {
+    console.log("Star selected:", star);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Background />
+
+      <SearchBar
+        value={search}
+        onChange={handleSearch}
+        onClear={handleClear}
+        onSubmit={handleSubmit}
+      />
+
+      {loading ? (
+        <ActivityIndicator style={styles.loader} color="#fff" />
+      ) : (
+        <ScrollView contentContainerStyle={styles.scrollContent}>
+          <StarsList stars={stars} onPressStar={handlePressStar} />
+        </ScrollView>
+      )}
+    </View>
+  );
+}
