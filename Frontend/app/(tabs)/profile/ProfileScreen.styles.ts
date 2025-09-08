7e8902bf1259acaec7a1df8385989e51e1748a0f
aliserhan315 <aliserhan315@gmail.com>
2025-09-09 00:51:44 +0300

    created a reusable button and created the style page to seperate the concerns

---
+++ Frontend/app/(tabs)/profile/ProfileScreen.styles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    backgroundColor: "#000",
+  },
+  content: {
+    paddingHorizontal: 16,
+  },
+});
