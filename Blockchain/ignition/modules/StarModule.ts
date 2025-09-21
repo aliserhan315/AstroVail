5c3db8deb406ed2edd40a101e5662e7e55e28080
aliserhan315 <aliserhan315@gmail.com>
2025-09-21 12:41:57 +0300

    removed the old way of blockchain

--- Blockchain/ignition/modules/StarModule.ts
+++
@@ -1,7 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-export default buildModule("StarModule", (m: any) => {
-  const admin = m.getParameter("admin");
-  const star = m.contract("StarOwnership", [admin]);
-  return { star };
-});
--- Blockchain/types/hardhat-plugins.d.ts
+++
@@ -1,5 +0,0 @@
-import "@nomicfoundation/hardhat-toolbox-viem";
-
-declare module "hardhat/types/runtime" {
-  // Augmentations for viem are provided by the toolbox; no custom additions required here.
-}
